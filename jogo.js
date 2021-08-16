8928a92464cea7f48e2f3568f9015d3f2d8d6d7c
João Paulo <joaopaulogomesdasilva99@gmail.com>
2021-08-16 12:55:44 -0300

    inicio do jogo

---
+++ jogo.js
@@ -0,0 +1,14 @@
+function Jao(){
+    document.write(`
+        background-image: url("hhttps://super.abril.com.br/wp-content/uploads/2019/09/fotosvencedora.png?quality=70&strip=info&resize=1920,1080")
+`)
+}
+function Beatris(){
+    document.write("deu certo")
+}
+function Brennerson(){
+    document.write("deu certo")
+}
+function Juse(){
+    document.write("deu certo")
+}
